5653fccd89d74bbf8483c6dbf6c2b9592408d380
mehdi <mehdi.hooshyaripour@gmail.com>
2021-02-23 12:57:38 +0330

    Update GasFlowHistory.py

--- GasFlowHistory.py
+++ GasFlowHistory.py
@@ -1,13 +1,11 @@
-import numpy as np
-import cv2 as cv
-import imutils as imutils
+import os
 import numpy as np
 from numpy.lib.index_tricks import MGridClass
 from matplotlib import pyplot as plt
 
 class GasFlowHistory:
 
-    def __init__(self, fps, nf, gas_region_shape, global_frame_counter):
+    def __init__(self, fps, nf, gas_region_shape, global_frame_time):
         self.fps = fps
         self.nf = nf
         self.max_gas_region_hist = int(10 * fps / nf)
@@ -18,12 +16,18 @@ class GasFlowHistory:
         self.gas_ended = False
 
         # Gas information
-        self.start_time = global_frame_counter / fps
-        self.stop_time = self.start_time
+        self.gas_id = os.getuid()
+        self.start_time = global_frame_time
+        self.stop_time = None
         self.rate_FCS = None
         self.gas_peak_index = None
 
-    def IncreaseTimeStep(self, global_frame_counter):
+        # Start-Stop Event Report
+        self.start_event_reported = False
+        self.stop_event_reported = False
+
+
+    def IncreaseTimeStep(self, global_frame_time):
         if self.gas_ended:
             return
         self.index_gas_region_hist += 1
@@ -33,7 +37,7 @@ class GasFlowHistory:
         self.last_gas_region_mean = np.mean(self.gas_region_hist, axis=0)
         if np.sum(self.last_gas_region_mean) == 0:
             self.gas_ended = True
-        self.stop_time = global_frame_counter / self.fps
+            self.stop_time = global_frame_time
 
 
     def IsInThisRegion(self, gas_region):
@@ -56,10 +60,38 @@ class GasFlowHistory:
         return 0
 
     def GetGasInfo(self):
-        info = {"start time" : np.round(self.start_time, 2),
-                "stop time" : np.round(self.stop_time, 2),
-                "rate FCM" : np.round(self.rate_FCS, 2),
-                "x" : np.int0(self.gas_peak_index[1]),
-                "y" : np.int0(self.gas_peak_index[0]) }
+        if (self.start_event_reported == True) and (self.stop_event_reported == True):
+            return None
+
+        # smoke started but It continues (in next period)
+        if self.stop_time is None:
+            info = {
+                "id": self.gas_id,
+                "start time": self.start_time,
+                "rate FCM": np.round(self.rate_FCS, 2),
+                "x": np.int0(self.gas_peak_index[1]),
+                "y": np.int0(self.gas_peak_index[0])}
+            self.start_event_reported = True
+        else:
+            # smoke started in previous period
+            if self.start_event_reported == True:
+                info = {
+                    "id": self.gas_id,
+                    "stop time": self.stop_time,
+                    "rate FCM": np.round(self.rate_FCS, 2),
+                    "x": np.int0(self.gas_peak_index[1]),
+                    "y": np.int0(self.gas_peak_index[0])}
+                self.stop_event_reported = True
+            else:
+                # smoke started and has stoped in this period
+                info = {
+                    "id": self.gas_id,
+                    "start time": self.start_time,
+                    "stop time": self.stop_time,
+                    "rate FCM": np.round(self.rate_FCS, 2),
+                    "x": np.int0(self.gas_peak_index[1]),
+                    "y": np.int0(self.gas_peak_index[0])}
+                self.start_event_reported = True
+                self.stop_event_reported = True
 
         return info
